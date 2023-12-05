ff3d5adea209efecbc03af4192b853399edcb70f
nakao <yukimiruku1126@gmail.com>
2023-12-05 22:59:25 +0900

    feat: :sparkles: NextUIの導入

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { useTranslation } from '@/i18n'
 import { Languages } from '@/i18n/settings'
+import { Button } from '@nextui-org/react'
 
 export type HomeProps = {
   params: {
@@ -35,6 +36,11 @@ export default async function Home({ params: { lng } }: HomeProps) {
             />
           </a>
         </div>
+      </div>
+      <div>
+        <Button color="primary" isLoading>
+          Loading Test Button
+        </Button>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
