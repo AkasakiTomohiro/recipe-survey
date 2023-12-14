fc448d94ccafda9bd03ed77eb8429bc41ec77280
a.kubo <ayuyellow1725@gmail.com>
2023-12-14 23:28:13 +0900

    test: :white_check_mark: スナップショットテストの設定追加＆カバレッジが100になるようにstoryファイル調整

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { within, userEvent, expect } from '@storybook/test';
+import { within, userEvent, expect, waitFor } from '@storybook/test';
 
 import { Page } from './Page';
 
@@ -15,17 +15,46 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const LoggedOut: Story = {};
+export const LoggedOut: Story = {
+  play: async ({ canvasElement }) => {
+    // 初期（ログアウト）状態
+    const canvas = within(canvasElement);
+    const loginButton = canvas.getByRole('button', { name: /Log in/i });
+    await expect(loginButton).toBeInTheDocument();
+    
+    await userEvent.click(loginButton); // ログイン
+    await expect(loginButton).not.toBeInTheDocument();
+    const logoutButton = canvas.getByRole('button', { name: /Log out/i });
+    await expect(logoutButton).toBeInTheDocument();
+    await userEvent.click(logoutButton); // ログアウト
+    const signUpButton = canvas.getByRole('button', { name: /Sign up/i });
+    await expect(signUpButton).toBeInTheDocument();
+    await expect(loginButton).not.toBeInTheDocument();
+  }
+};
 
 // More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
 export const LoggedIn: Story = {
   play: async ({ canvasElement }) => {
+    // 初期（ログアウト）状態
     const canvas = within(canvasElement);
     const loginButton = canvas.getByRole('button', { name: /Log in/i });
     await expect(loginButton).toBeInTheDocument();
-    await userEvent.click(loginButton);
+    await userEvent.click(loginButton); // ログイン
     await expect(loginButton).not.toBeInTheDocument();
+    const logoutButton = canvas.getByRole('button', { name: /Log out/i });
+    await expect(logoutButton).toBeInTheDocument();
+  },
+};
 
+export const SignUp: Story = {
+  play: async ({ canvasElement }) => {
+    // 初期（ログアウト）状態
+    const canvas = within(canvasElement);
+    const signUpButton = canvas.getByRole('button', { name: /Sign up/i });
+    await expect(signUpButton).toBeInTheDocument();
+    await userEvent.click(signUpButton); // サインアップ
+    await expect(canvas.queryByRole("button", { name: /Sign up/i })).not.toBeInTheDocument();
     const logoutButton = canvas.getByRole('button', { name: /Log out/i });
     await expect(logoutButton).toBeInTheDocument();
   },
