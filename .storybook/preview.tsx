4215b1889abfdd335b79e5526af1b7a178ba3742
t.akasaki <t.akasaki.csharp@gmail.com>
2024-01-25 22:45:47 +0900

    feat: :sparkles: StorybookにI18Nを追加

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,26 @@
 import type { Preview } from "@storybook/react";
+import { NextIntlClientProvider } from 'next-intl';
 import React from 'react';
+import i18n from 'storybook-i18n/preview';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
+import enJson from '../src/i18n/locales/en.json';
+import jpJson from '../src/i18n/locales/ja.json';
 import { lightTheme } from '../src/theme/LightTheme';
 
+// @ts-ignore
+const i18nDecorators = i18n?.decorators || [];
+
+// 翻訳マップ
+const messages = {
+  en: enJson,
+  ja: jpJson
+};
+
 const preview: Preview = {
-  parameters: {
+  ...i18n,
+ parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
@@ -15,17 +29,26 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    ...i18nDecorators,
+    (Story, { globals }) => {
+      return (
+        <NextIntlClientProvider locale={globals.locale} messages={messages[globals.locale]}>
+          <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Story />
+          </ThemeProvider>
+        </NextIntlClientProvider>
+      );
+    }
+  ],
+  globals: {
+    locale: "ja",
+    locales: {
+        en: "English",
+        ja: "日本語"
+    }
+  }
 };
 
 export default preview;
-
-export const decorators = [
-  (Story: () => JSX.Element) => {
-    return (
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Story />
-      </ThemeProvider>
-    )
-  },
-]
