c7a4fb859deaf5ae091fec97c98793393445c56d
t.akasaki <t.akasaki.csharp@gmail.com>
2024-02-08 23:18:00 +0900

    feat: :sparkles: アイコン用のコンポーネントを追加

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @file index.ts
+ * @author Artan's Projects
+ * @copyright © 2024 Artan's Projects. All rights reserved.
+ */
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+export { 
+  KeyboardArrowDownIcon, 
+  KeyboardArrowUpIcon
+};
