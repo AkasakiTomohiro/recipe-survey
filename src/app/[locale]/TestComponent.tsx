4d82be29e03b74d156207e2185dbeb2c4e1f4e36
a.kubo <ayuyellow1725@gmail.com>
2024-03-21 21:07:41 +0900

    refactor: :recycle: appフォルダ配下をfeaturesフォルダに移動

--- src/app/[locale]/TestComponent.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useProfile } from '@/providers';
-
-export const TestComponent = () => {
-  const { getRecipes } = useProfile();
-  return <div>{JSON.stringify(getRecipes())}</div>;
-}
